d046dfad54d8a1427959b951f489e51ddc41ac4e
gandhia <animesh.gandhi@earlywarning.com>
2023-02-04 23:01:39 -0700

    Created a new API for pokemon image

---
+++ pokemon.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, make_response
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/<pokemon>")
+def getPokemon(pokemon):
+    pokeAPI = f"https://pokeapi.co/api/v2/pokemon-form/{pokemon}"
+    pokeAPIResponse = ""
+
+    try:
+        pokeAPIResponse = requests.get(pokeAPI).json()
+    except BaseException:
+        error = {
+            "code": 100,
+            "description": "Error fetching data from Pokemon API"
+        }
+        return jsonify(error)
+    
+    sprites = pokeAPIResponse["sprites"]
+    frontImage = sprites["front_default"]
+    response = make_response({
+        "name": f"{pokemon}",
+        "image": frontImage
+    })
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+    
+
